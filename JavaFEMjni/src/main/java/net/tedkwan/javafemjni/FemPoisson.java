e5182cd962077b5ff73d81ae774a81393a33aba8
tmkwan <tkwan@impulse.net>
2017-02-27 02:46:00 -0800

    One More addition

---
+++ JavaFEMjni/src/main/java/net/tedkwan/javafemjni/FemPoisson.java
@@ -0,0 +1,49 @@
+
+
+
+package net.tedkwan.javafemjni;
+
+import java.io.IOException;
+
+/**
+ * This class is to interface with C++ using JNI.
+ * 
+ * It calls the cFEM library in C++ and runs the JNI
+ * method.
+ * 
+ * @author Ted Kwan
+ */
+public class FemPoisson {
+    /**
+     * Access Native Library.
+     */
+    static {   
+        try {    
+            NativeUtils.loadLibraryFromJar("/lib/libcFEM.so");   
+        } catch (IOException e) {
+            System.out.println("Failed to load lib.");
+        }
+    }
+
+    /**
+     * This is the native method which calls C++
+     * 
+     * Pars has 5 fields:
+     * 
+     * x lower bound - index 0.
+     * x upper bound - index 1.
+     * y lower bound - index 2.
+     * y upper bound - index 3.
+     * h - index 4.
+     * 
+     * @param pars This is the double array passed to C++
+     * @param femfun This is a string to pass to C++, it represents the name of the function to be run.
+     * @return double array which represents x,y and u. This needs to be split into a Nx3 matrix to be parsed.
+     */
+    public native double[] cFEM(double[] pars,String femfun);
+    
+    public FemPoisson(){
+        
+    }
+    
+}
